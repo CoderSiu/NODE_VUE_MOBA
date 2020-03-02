364d1d6eb51203837cf59f7a442b24181817dd75
CoderSiu <bin.siu@icloud.com>
2020-03-02 11:36:59 +0800

    adminuser finished

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -12,6 +12,8 @@ import ArticleEdit from '../views/ArticleEdit.vue'
 import ArticleList from '../views/ArticleList.vue'
 import AdEdit from '../views/AdEdit.vue'
 import AdList from '../views/AdList.vue'
+import AdminUserEdit from '../views/AdminUserEdit.vue'
+import AdminUserList from '../views/AdminUserList.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -85,6 +87,20 @@ const routes = [
       {
         path: '/ads/edit/:id',
         component: AdEdit,
+        props: true
+      },
+
+      {
+        path: '/admin_users/create',
+        component: AdminUserEdit
+      },
+      {
+        path: '/admin_users/list',
+        component: AdminUserList
+      },
+      {
+        path: '/admin_users/edit/:id',
+        component: AdminUserEdit,
         props: true
       },
     ]
---
+++ admin/src/views/AdminUserList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <h1>分类列表</h1>
+      <el-table :data="items">
+        <el-table-column prop="_id" label="ID" width="180"></el-table-column>
+        <el-table-column prop="name" label="用户名"></el-table-column>
+        <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="$router.push(`/admin_users/edit/${scope.row._id}`)">编辑</el-button>
+            <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      items: []
+    }
+  },
+  methods:{
+    async fetch() {
+      const res = await this.$http.get('rest/admin_users')
+      this.items = res.data
+    },
+    async remove(row) {
+      this.$confirm(`此操作将永久删除该文件, 是否继续删除${row.name} ?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+      }).then( async () => {
+        await this.$http.delete(`rest/admin_users/${row._id}`)
+        this.fetch()
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });          
+      })
+      
+      }
+  },
+  created() {
+    this.fetch()
+  }
+}
+</script>
